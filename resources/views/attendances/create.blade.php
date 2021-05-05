60184161c42ffbc270e861ce7dccbe5ddb0b7910
virtualLantner <marek.lunter02@gmail.com>
2021-05-05 19:32:53 +0200

    Attendance controller and views added

---
+++ resources/views/attendances/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+
+main obsah - attendance>create.blade.php -> stranka pre vytvorenie attendance(formular)
+
+@endsection
---
+++ resources/views/attendances/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+
+ main obsah - attendance>index.blade.php -> uvodna stranka pre attendances- vypisanie vsetkych attendances prhlaseneu userovi
+
+@endsection
